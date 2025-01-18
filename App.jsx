502e4b67797cd31d45ded6094416ae87feafd939
khadiza21 <bibikhadiza474@gmail.com>
2025-01-18 17:08:02 +0300

    native first app

---
+++ App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react-native/no-inline-styles */
+
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Khadiza is Nice </Text>
+      <Text style={{ fontSize: 20 }}>Khadiza is  Smart</Text>
+      <Button title="Press Here" />
+    </View>
+  );
+};
+
+export default App;
