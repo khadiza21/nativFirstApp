8e07fb4bf8373a6694fb2395dc5611e231f5d597
khadiza21 <bibikhadiza474@gmail.com>
2025-01-19 10:29:51 +0300

    first component test

--- App.jsx
+++ App.jsx
@@ -1,13 +1,16 @@
 /* eslint-disable react-native/no-inline-styles */
 
+// rnfe 
 import React from 'react';
 import { View, Text, Button } from 'react-native';
+import First from './src/components/First';
 const App = () => {
   return (
     <View>
       <Text style={{ fontSize: 30 }}>Khadiza is Nice </Text>
       <Text style={{ fontSize: 20 }}>Khadiza is  Smart</Text>
       <Button title="Press Here" />
+      <First />
     </View>
   );
 };
---
+++ src/components/First.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react-native/no-inline-styles */
+/* eslint-disable semi */
+import React from 'react'
+import { Button, Text, View } from 'react-native'
+
+const First = () => {
+    const getName = (name) => {
+        console.warn('Function 1');
+        console.log('khadiza')
+    }
+    const getAge = () => {
+        console.warn('Function 2');
+        console.log('age: 20')
+    }
+    return (
+
+        <View>
+            <Text style={{ fontSize: 20 }}>First component are making. </Text>
+
+            <Button title='Press 1' onPress={() => getName('Khadiza')}  />
+
+            <Text style={{ fontSize: 25 }}>Button two</Text>
+            <Button title='Press 2' onPress={getAge} />
+        </View>
+    )
+}
+
+// eslint-disable-next-line no-trailing-spaces, eol-last
+export default First; 
